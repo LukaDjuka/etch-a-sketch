dd9b810423bc01ca2c96ebde99b21cae7c02126f
Luka Djukic <luka.djukic10@gmail.com>
2021-03-03 18:52:52 -0500

    Fixing reset button to redraw proper amount of grid items

--- scripts.js
+++ scripts.js
@@ -2,18 +2,33 @@ let resetButton = document.querySelector("#reset");
 
 
 let gridContainer = document.querySelector("#container");
-for (let i = 0; i < 256; i++){
-    let gridBox = document.createElement("div");
-    gridBox.classList.add("uniform-grid-item");
-    gridBox.addEventListener('mouseover', function () {
-    this.style.backgroundColor = "chocolate";
-    });
-    gridContainer.appendChild(gridBox);
+
+
+function createGrid(columns, rows){
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    for (let i = 0; i < columns * rows; i++){
+        let gridBox = document.createElement("div");
+        gridBox.classList.add("uniform-grid-item");
+        gridBox.addEventListener('mouseover', function () {
+        this.style.backgroundColor = "chocolate";
+        });
+        gridContainer.appendChild(gridBox);
+    }
 }
+
+createGrid(16,16);
+
 let gridItems = document.querySelectorAll(".uniform-grid-item");
 
 resetButton.addEventListener("click", resetGrid);
 
 function resetGrid(){
     gridItems.forEach(item => item.style.backgroundColor = "moccasin");
+    let numCols = prompt("Please enter an integer for the amount of Columns");
+    let numRows = prompt("Please enter an integer for the amount of Rows");
+    gridContainer.style.gridTemplateColumns = `repeat(${numCols}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${numRows}, 1fr)`;
+    createGrid(numCols, numRows);
 }
